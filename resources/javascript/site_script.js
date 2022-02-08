bc7a6b860a373575910ba69d53942915f6b09b02
ZacRobinson84 <zacrobinson84@gmail.com>
2022-02-07 20:06:12 -0500

    begin styling animated background to preference

--- resources/javascript/site_script.js
+++ resources/javascript/site_script.js
@@ -1,23 +1,17 @@
 document.body.onload = addElement;
 
 function addElement() {
-  for (let i = 0; i < 10; i++) {
-    setTimeout(() => {
-      let randNumY = Math.floor(Math.random() * 1000);
+  for (let i = 0; i < 30; i++) {
+    let randNumY = Math.floor(Math.random() * 3000);
+    let randDelay = Math.floor(Math.random() * 3000);
+    let randDuration = Math.floor(Math.random() * 1000);
+    const newDiv = document.createElement("div");
+    newDiv.className = "banana";
 
-      let randDelay = Math.floor(Math.random() * 5000);
-      let randDuration = Math.floor(Math.random() * 5000);
-      const newDiv = document.createElement("div");
-      newDiv.className = "banana";
-      const newContent = document.createTextNode("TESTER TESTER TESTER");
-      newDiv.appendChild(newContent);
+    newDiv.style.top = randNumY.toString().concat("px");
+    newDiv.style.animationDuration = randDuration.toString().concat("ms");
+    newDiv.style.animationDelay = randDelay.toString().concat("ms");
 
-      newDiv.style.top = randNumY.toString().concat("px");
-
-      newDiv.style.animationDelay = randDelay.toString().concat("ms");
-      newDiv.style.animationDuration = randDuration.toString().concat("ms");
-
-      document.body.appendChild(newDiv);
-    }, Math.floor(Math.random() * 5000));
+    document.body.appendChild(newDiv);
   }
 }
