cbe779a179caff9aa9f0ffd340d550cbdd71eaa1
ZacRobinson84 <zacrobinson84@gmail.com>
2022-02-08 19:50:35 -0500

    style background intro animation to preference

--- resources/javascript/site_script.js
+++ resources/javascript/site_script.js
@@ -1,27 +1,39 @@
+document.body.onload = addElement;
+
 function getRandNum(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function createDivs(divName, rotateRoll) {
-  for (let i = 0; i < 15; i++) {
+function getRandRGB() {
+  let a = getRandNum(0, 255).toString();
+  let b = getRandNum(0, 255).toString();
+  let c = getRandNum(0, 255).toString();
+  let rgbValue = `rgb(${a}, ${b}, ${c})`;
+  return rgbValue;
+}
+
+function createDivs(divClass, divID, rotateRoll) {
+  for (let i = 0; i < 10; i++) {
     setTimeout(() => {
-      let randNumY = getRandNum(0, 2000);
-      let randDelay = getRandNum(0, 1500);
-      let randDuration = getRandNum(500, 1000);
+      let randNumY = getRandNum(50, 1100);
+      let randDelay = getRandNum(0, 1000);
+      let randDuration = getRandNum(300, 1000);
       let randRotate = getRandNum(0, rotateRoll);
       const newDiv = document.createElement("div");
-      newDiv.className = divName;
+      newDiv.className = divClass;
+      newDiv.id = divID;
 
+      newDiv.style.backgroundColor = getRandRGB();
       newDiv.style.top = randNumY.toString().concat("px");
       newDiv.style.animationDuration = randDuration.toString().concat("ms");
       newDiv.style.animationDelay = randDelay.toString().concat("ms");
       newDiv.style.transform = "rotate(" + randRotate + "deg)";
-      document.body.appendChild(newDiv);
-    }, getRandNum(0, 2000));
+      document.getElementById("tester").appendChild(newDiv);
+    }, getRandNum(0, 3000));
   }
 }
 
 function addElement() {
-  createDivs("banana", 11);
-  createDivs("orange", -11);
+  createDivs("banana", "left", 8);
+  createDivs("banana", "right", -8);
 }
