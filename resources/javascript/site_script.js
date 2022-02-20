ce5f0ffba064c7281076351905893e9e0af7218c
ZacRobinson84 <zacrobinson84@gmail.com>
2022-02-19 20:57:12 -0500

    possibly scrap idea to make drop menu link animation similar to intro stripe animation

--- resources/javascript/site_script.js
+++ resources/javascript/site_script.js
@@ -10,7 +10,14 @@ document.getElementById("menuIcon").addEventListener("click", () => {
   addClass("dropMenuBackground", "drop-menu-background");
   addClass("dropMenu", "drop-menu");
   addClass("menuIcon", "menu-icon");
-  createDivs("drop-menu-stripes", "dropMenuStripes", 4, 8, "dropMenu");
+  if (document.getElementById("menuIcon").classList.contains("revert")) {
+    setTimeout(() => {
+      let stripes = document.getElementById("stripesContainer");
+      while (stripes.firstChild) {
+        stripes.removeChild(stripes.lastChild);
+      }
+    }, 1000);
+  }
 });
 
 //Remove element eventlisteners
@@ -23,6 +30,26 @@ document.getElementById("navbar").addEventListener("animationend", () => {
   animateDivs("menuIcon");
 });
 
+document
+  .getElementById("dropMenuBackground")
+  .addEventListener("transitionend", () => {
+    if (!document.getElementById("menuIcon").classList.contains("revert")) {
+      createDivs(
+        "drop-menu-stripes",
+        "dropMenuStripes",
+        10,
+        8,
+        "stripesContainer",
+        1000,
+        2000,
+        300,
+        700,
+        getRandRGB,
+        500
+      );
+    }
+  });
+
 function myInit() {
   addElement();
 }
@@ -33,6 +60,7 @@ function getRandNum(min, max) {
 }
 
 // create a random rgb color string
+
 function getRandRGB() {
   let a = getRandNum(0, 255).toString();
   let b = getRandNum(0, 255).toString();
@@ -42,31 +70,67 @@ function getRandRGB() {
 }
 
 // generate a series of styled divs for background intro animation
-function createDivs(divClass, divID, divAmount, rotateRoll, targetId) {
+function createDivs(
+  divClass,
+  divID,
+  divAmount,
+  rotateRoll,
+  targetId,
+  yMax,
+  delay,
+  durationLow,
+  durationHigh,
+  divColor,
+  creationDelay
+) {
   for (let i = 0; i < divAmount; i++) {
     setTimeout(() => {
-      let randNumY = getRandNum(0, 1100);
-      let randDelay = getRandNum(0, 1000);
-      let randDuration = getRandNum(300, 1000);
+      let randNumY = getRandNum(0, yMax);
+      let randDelay = getRandNum(0, delay);
+      let randDuration = getRandNum(durationLow, durationHigh);
       let randRotate = getRandNum(0, rotateRoll);
       const newDiv = document.createElement("div");
       newDiv.className = divClass;
       newDiv.id = divID;
 
-      newDiv.style.backgroundColor = getRandRGB();
+      newDiv.style.backgroundColor = divColor();
       newDiv.style.top = randNumY.toString().concat("px");
       newDiv.style.animationDuration = randDuration.toString().concat("ms");
       newDiv.style.animationDelay = randDelay.toString().concat("ms");
       newDiv.style.transform = "rotate(" + randRotate + "deg)";
       document.getElementById(targetId).appendChild(newDiv);
-    }, getRandNum(0, 3000));
+    }, getRandNum(0, creationDelay));
   }
 }
 
 // divide the background intro divs into left and right starting positions
 function addElement() {
-  createDivs("banana", "left", 10, 8, "tester");
-  createDivs("banana", "right", 10, -8, "tester");
+  createDivs(
+    "banana",
+    "left",
+    10,
+    8,
+    "tester",
+    1100,
+    1000,
+    300,
+    1000,
+    getRandRGB,
+    3000
+  );
+  createDivs(
+    "banana",
+    "right",
+    10,
+    -8,
+    "tester",
+    1100,
+    1000,
+    300,
+    1000,
+    getRandRGB,
+    3000
+  );
 }
 
 // Add new class to an element
